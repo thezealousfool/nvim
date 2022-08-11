912161bba2f290e3a3362bdb43cec94b34ce0f0d
Vivek Roy <vivek_roy@apple.com>
2022-08-11 14:35:39 -0700

    Folding autocmds

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,6 +1,13 @@
 local M = {}
 
 function M:setup()
+  local fold_group = vim.api.nvim_create_augroup("Fold", { clear = true })
+  vim.api.nvim_create_autocmd({ "FileType", "BufWritePost" }, {
+    callback = function()
+      vim.cmd("normal zx")
+    end,
+    group = fold_group,
+  })
 end
 
 return M
