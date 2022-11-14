5a6b51bef7f8fecbcd21f8e68b59f511ea8df944
Vivek Roy <vivek_roy@apple.com>
2022-11-14 13:25:18 -0800

    gitsign customizations

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -120,7 +120,26 @@ function M:load()
     use {
       "lewis6991/gitsigns.nvim",
       config = function()
-        require("gitsigns").setup()
+        require("gitsigns").setup {
+          on_attach = function(bufnr)
+            local gs = package.loaded.gitsigns
+            local nav_ok, nav = pcall(require, "nvim-tmux-navigation")
+            vim.keymap.set({"n", "v"}, "<leader>hs", ":Gitsigns stage_hunk<CR>")
+            vim.keymap.set({"n", "v"}, "<leader>hr", ":Gitsigns reset_hunk<CR>")
+            vim.keymap.set("n", "<leader>gb", gs.toggle_current_line_blame)
+            vim.keymap.set("n", "<leader>gB", gs.blame_line)
+            vim.keymap.set("n", "]c", function()
+              if vim.wo.diff then return "]c" end
+              vim.schedule(function() gs.next_hunk() end)
+              return "<Ignore>"
+            end, { expr=true })
+            vim.keymap.set("n", "[c", function()
+              if vim.wo.diff then return "[c" end
+              vim.schedule(function() gs.prev_hunk() end)
+              return "<Ignore>"
+            end, { expr=true })
+          end
+        }
       end
     }
 
