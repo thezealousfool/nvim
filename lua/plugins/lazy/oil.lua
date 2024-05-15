980cf8f50996bd2bd7eef8737e05290335608f41
Vivek Roy <thezealousfool@gmail.com>
2024-05-15 10:56:39 -0700

    Add oil.nvim plugin

---
+++ lua/plugins/lazy/oil.lua
@@ -0,0 +1,16 @@
+return {
+	"stevearc/oil.nvim",
+	config = function()
+		require("oil").setup({
+			keymaps = {
+				["<C-x>"] = "actions.select_split",
+				["<C-v>"] = "actions.select_vsplit",
+        ["-"] = "actions.parent",
+        ["<CR>"] = "actions.select",
+			},
+      use_default_keymaps = false,
+		})
+		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+	end,
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+}
