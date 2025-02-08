7706784b534dceb588f555c0fd601478a31a3318
Vivek Roy <thezealousfool@gmail.com>
2025-02-08 01:57:59 -0800

    Overseer keybinds

--- lua/plugins/lazy/overseer.lua
+++ lua/plugins/lazy/overseer.lua
@@ -1,23 +1,51 @@
 return {
 	"stevearc/overseer.nvim",
+	event = { "VeryLazy" },
+	cmd = { "OverseerToggle" },
 	keys = {
 		{
-			"<leader>or",
+			"<leader>o",
 			function()
 				local overseer = require("overseer")
-				local tasks = overseer.list_tasks({ recent_first = true })
-				if vim.tbl_isempty(tasks) then
-					overseer.run_template()
-				else
-					overseer.run_action(tasks[1], "restart")
-				end
+				overseer.toggle()
 			end,
-			desc = "Run last",
+			desc = "Toggle overseer",
 		},
 	},
 	config = function()
 		require("overseer").setup({
 			strategy = "toggleterm",
+			task_list = {
+				bindings = {
+					["?"] = "ShowHelp",
+					["g?"] = false,
+					["<CR>"] = "RunAction",
+					["<C-e>"] = "Edit",
+					["o"] = "Open",
+					["<C-v>"] = false,
+					["<C-s>"] = false,
+					["<C-f>"] = false,
+					["<C-q>"] = false,
+					["p"] = false,
+					["<C-l>"] = false,
+					["<C-h>"] = false,
+					["L"] = false,
+					["H"] = false,
+					["["] = false,
+					["]"] = false,
+					["{"] = "PrevTask",
+					["}"] = "NextTask",
+					["<C-k>"] = false,
+					["<C-j>"] = false,
+					["q"] = false,
+					["l"] = "<cmd>OverseerLoadBundle!<cr>",
+					["s"] = "<cmd>OverseerQuickAction start<cr>",
+					["r"] = "<cmd>OverseerQuickAction restart<cr>",
+					["w"] = "<cmd>OverseerSaveBundle<cr>",
+					["d"] = "<cmd>OverseerDeleteBundle<cr>",
+					["o"] = "<cmd>OverseerBuild<cr>",
+				},
+			},
 		})
 	end,
 }
