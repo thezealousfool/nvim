700e3cce7deb332eb1a60be4a4bf9ef27b156900
Vivek Roy <thezealousfool@gmail.com>
2025-04-29 11:28:25 +0530

    Add open-external in oil.nvim

--- lua/plugins/lazy/oil.lua
+++ lua/plugins/lazy/oil.lua
@@ -1,3 +1,17 @@
+open_external = {
+  desc = "Open the entry under the cursor in an external program",
+  callback = function()
+    local oil = require("oil")
+    local entry = oil.get_cursor_entry()
+    local dir = oil.get_current_dir()
+    if not entry or not dir then
+      return
+    end
+    local path = dir .. entry.name
+    vim.ui.open(path)
+  end,
+}
+
 return {
 	"stevearc/oil.nvim",
 	config = function()
@@ -8,6 +22,7 @@ return {
         ["-"] = "actions.parent",
         ["<CR>"] = "actions.select",
         ["g."] = "actions.toggle_hidden",
+        ["gx"] = open_external,
 			},
       use_default_keymaps = false,
 		})
